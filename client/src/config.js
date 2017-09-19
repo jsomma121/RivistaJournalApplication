f41bccae150daeb4549a95b6e897d4e71bd464ca
Joshua Somma <jsomma121@gmail.com>
2017-09-19 22:37:35 +1000

    Address problem with AWS congnito and signup

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export default {
     cognito: {
-      USER_POOL_ID: "ap-southeast-2_v0aRipTP7",
-      APP_CLIENT_ID: "5jh1m03p57sqboo1vi9kf1sht6"
+      USER_POOL_ID: "ap-southeast-2_1xqhxEidk",
+      APP_CLIENT_ID: "2jjs6k1qnr15qgp8uev61gkoom"
     }
   };
