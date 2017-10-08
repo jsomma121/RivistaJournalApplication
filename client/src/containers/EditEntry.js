c473b1f9fb2ca05d498f5473ba4052597417d520
Yahya <yahya.zein@student.uts.edu.au>
2017-10-08 22:35:11 +1100

    Edit entry functionality done?

--- client/src/containers/EditEntry.js
+++ client/src/containers/EditEntry.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-dom';
-import { Editor, EditorState, RichUtils } from 'draft-js';
 import Octoicon from 'react-octicon';
+import { Tooltip } from 'react-bootstrap';
 import moment from 'moment';
 import { invokeApig } from '../libs/awsLib';
 import LoaderButton from "../components/LoaderButton";
@@ -12,73 +12,146 @@ export default class EditEntry extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      editorState: EditorState.createEmpty(),
-      content: ''
+      isLoading: true,
+      title: "",
+      reason: "Created",
+      content: '',
+      entry: null,
+      revision: null,
+      existingEntry: false,
+      error: false
     };
-
-    this.focus = () => this.refs.editor.focus();
-    this.onChange = (editorState) => this.setState({ editorState });
-
-    this.handleKeyCommand = (command) => this._handleKeyCommand(command);
-    this.onTab = (e) => this._onTab(e);
-    this.toggleBlockType = (type) => this._toggleBlockType(type);
-    this.toggleInlineStyle = (style) => this._toggleInlineStyle(style);
-
   }
 
-  _handleKeyCommand(command) {
-    const { editorState } = this.state;
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
+  componentDidMount() {
+    console.log(this.props.currentJournal);
+    if (this.state.isLoading) {
+      var entry = this.getEntry();
+      var revision = this.getRevision();
+      console.log(entry);
+      if (entry != null) {
+        if (revision == null) {
+          this.setState({
+            entry: entry,
+            title: entry.title,
+            reason: "",
+            content: entry.revision[0].content,
+            existingEntry: true
+          })
+          this.props.updateChildProps({
+            currentEntry: entry,
+            currentJournal: this.props.currentJournal,
+            currentEntryRevision: null
+          });
+        } else {
+          this.setState({
+            entry: entry,
+            title: revision.title,
+            reason: "",
+            content: revision.content,
+            existingEntry: true
+          })
+        }
+      }
+
+      this.setState({ isLoading: false });
     }
-    return false;
   }
 
-  _onTab(e) {
-    const maxDepth = 4;
-    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
+  getEntry() {
+    if (this.props.currentJournal != null) {
+      var entries = this.props.currentJournal.enteries;
+      for (var i = 0; i < entries.length; i++) {
+        if (entries[i].entryId === this.props.match.params.entryId) {
+          return entries[i];
+        }
+      }
+      return null;
+    }
+    return null;
   }
 
-  _toggleBlockType(blockType) {
-    this.onChange(
-      RichUtils.toggleBlockType(
-        this.state.editorState,
-        blockType
-      )
-    );
+  getRevision() {
+    if (this.state.entry != null) {
+      var revisions = this.state.entry.revision;
+      for (var i = 0; i < revisions.length; i++) {
+        if (revisions[i].revisionId === this.props.match.params.entryId) {
+          return revisions[i];
+        }
+      }
+      return null;
+    }
+    return null;
   }
 
-  _toggleInlineStyle(inlineStyle) {
-    this.onChange(
-      RichUtils.toggleInlineStyle(
-        this.state.editorState,
-        inlineStyle
-      )
-    );
+  onTitleChange = event => {
+    this.setState({
+      title: event.target.value
+    })
+  }
+
+  onReasonChange = event => {
+    this.setState({
+      reason: event.target.value
+    })
+  }
+
+  onChange = event => {
+    this.setState({
+      content: event.target.value
+    })
+  }
+
+  validateForm() {
+    return this.state.content.length > 0 && this.state.title.length > 0 && this.state.reason.length > 0
   }
 
   handleSubmit = async event => {
     event.preventDefault();
-	const { editorState } = this.state;
-	console.log(this.props);
-    console.log(this.props.currentJournal.enteries);
-    var contentState = editorState.getCurrentContent();
-    this.props.currentJournal.enteries.unshift({
-      entryId: uuid.v1(),
-      title: "New Entry",
-      content: contentState.getBlockMap().first().text,
-      state: 'active',
-      createdAt: moment().utc().format(),
-      updatedAt: moment().utc().format(),
-      revision: []
-    });    
+    
+    if (this.state.entry == null) {
+      if (this.props.currentJournal.enteries.findIndex(e => e.title === this.state.title) != -1) {
+        this.setState({
+          error: true
+        })
+        return
+      }
+      this.props.currentJournal.enteries.push({
+        entryId: uuid.v1(),
+        title: this.state.title,
+        state: 'active',
+        createdAt: moment().utc().format(),
+        updatedAt: moment().utc().format(),
+        revision: [{
+          revisionId: uuid.v1(),
+          reason: this.state.reason,
+          content: this.state.content,
+          modificationAt: moment().utc().format()
+        }]
+      });
+    } else {
+      var pos = this.props.currentJournal.enteries.findIndex(e => e.entryId === this.state.entry.entryId);
+      this.props.currentJournal.enteries[pos].updatedAt = moment().utc().format();
+      this.props.currentJournal.enteries[pos].revision.unshift({
+        revisionId: uuid.v1(),
+        reason: this.state.reason,
+        content: this.state.content,
+        modificationAt: moment().utc().format()
+      })
+    }
 
     try {
-      const update = this.updateJournal(this.props.currentJournal);
+      const update = await this.updateJournal(this.props.currentJournal);
     } catch (e) {
       this.setState({ isLoading: false });
+    }
+  }
+
+  handleCancel = event => {
+    if (this.state.revision === null) {
+      this.props.history.push("/entry/" + this.props.match.params.entryId);
+    } else {
+      this.props.history.push("/history/" + this.props.match.params.entryId);
     }
   }
 
@@ -86,112 +159,53 @@ export default class EditEntry extends React.Component {
     invokeApig({
       path: "/journal/" + journal.journalid,
       method: "PUT",
-      body: {enteries: journal.enteries}
+      body: { enteries: journal.enteries }
     })
   }
 
-  render() {
-    const { editorState } = this.state;
-
-    let className = 'RichEditor-editor';
-
-    var contentState = editorState.getCurrentContent();
-
-    if (!contentState.hasText()) {
-      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
-        className += ' RichEditor-hidePlaceholder';
-      }
+  renderTitleInput() {
+    if (!this.state.error) {
+      return (
+        <div className="form-group edit-title">
+          <input type="text" className="form-control" id="title" onChange={this.onTitleChange} value={this.state.title} placeholder="Title" disabled={this.state.existingEntry} />
+        </div>
+      )
+    } else {
+      return (
+        <div className="form-group has-danger edit-title">
+          <input type="text" className="form-control" id="title" onChange={this.onTitleChange} value={this.state.title} placeholder="Title" disabled={this.state.existingEntry} />
+          <Tooltip placement="bottom" className="in" id="error">
+            Sorry, that title is taken.
+          </Tooltip>
+        </div>
+      )
     }
+  }
 
+  render() {
     return (
       <div>
-        <h1>Entry</h1>
-        <br />
+        {this.renderTitleInput()}
+        <div className="form-group">
+          <h2><label htmlFor="reason">Reason</label></h2>
+          <input type="text" className="form-control" id="reason" onChange={this.onReasonChange} value={this.state.reason} placeholder="Reason for change" disabled={this.state.revision !== null} />
+        </div>
         <h2>Content</h2>
         <form onSubmit={this.handleSubmit}>
-          <div className="RichEditor-root">
-            <InlineStyleControls
-              editorState={editorState}
-              onToggle={this.toggleInlineStyle}
-            />
-            <div className={className} onClick={this.focus}>
-              <Editor
-                customStyleMap={styleMap}
-                editorState={editorState}
-                handleKeyCommand={this.handleKeyCommand}
-                onChange={this.onChange}
-                onTab={this.onTab}
-                ref="editor"
-                spellCheck={true}
-              />
-            </div>
+          <textarea type="text" className="edit-input" onChange={this.onChange} value={this.state.content} disabled={this.state.revision !== null} />
+          <div className="edit-buttons">
+            <LoaderButton
+              type="submit"
+              isLoading={this.state.isLoading}
+              disabled={!this.validateForm()}
+              className="btn-success"
+              text="Save Entry"
+              loadingText="Creating..." />
+            <button type="button" className="btn btn-secondary" onClick={this.handleCancel}>Cancel</button>
           </div>
-          <LoaderButton
-            type="submit"
-            isLoading={this.state.isLoading}
-            className="btn-primary"
-            text="Create Journal"
-            loadingText="Creating..." />
-          <button type="button" className="btn btn-secondary"  >Cancel</button>
         </form>
       </div>
 
     );
   }
 }
-
-// Custom overrides for "code" style.
-const styleMap = {
-  CODE: {
-    backgroundColor: 'rgba(0, 0, 0, 0.05)',
-    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
-    fontSize: 16,
-    padding: 2,
-  },
-};
-
-class StyleButton extends React.Component {
-  constructor() {
-    super();
-    this.onToggle = (e) => {
-      e.preventDefault();
-      this.props.onToggle(this.props.style);
-    };
-  }
-
-  render() {
-    let className = 'RichEditor-styleButton';
-    if (this.props.active) {
-      className += ' RichEditor-activeButton';
-    }
-
-    return (
-      <span className={className} onMouseDown={this.onToggle}>
-        {this.props.label}
-      </span>
-    );
-  }
-}
-
-var INLINE_STYLES = [
-  { label: 'B', style: 'BOLD' },
-  { label: 'I', style: 'ITALIC' },
-  { label: 'U', style: 'UNDERLINE' },
-];
-
-const InlineStyleControls = (props) => {
-  var currentStyle = props.editorState.getCurrentInlineStyle();
-  return (
-    <div className="RichEditor-controls">
-      {INLINE_STYLES.map(type =>
-        <StyleButton
-          key={type.label}
-          active={currentStyle.has(type.style)}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  );
-};
