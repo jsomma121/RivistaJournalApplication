d0fcf8c07ce9c52c98fd4fb66403d34255a87ea7
Yahya <yahya.zein@student.uts.edu.au>
2017-10-13 23:35:07 +1100

    Help & Settings pages too

---
+++ client/src/containers/Help.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Help extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>In construction</div>
+    }
+}
---
+++ client/src/containers/Settings.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+export default class Settings extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    handleLightTheme() {
+        var theme = {
+            primary: "#FFF",
+            background: "#F1F1F1",
+            text: "#000",
+            shadow: "",
+            input: ""
+        }
+        this.props.updateTheme(theme);
+    }
+
+    handleDarkTheme() {
+        var theme = {
+            primary: "#36393E",
+            background: "#1E2124",
+            text: "#FFF",
+            shadow: "dark-shadow",
+            input: "dark-input"
+        }
+        this.props.updateTheme(theme);
+    }
+
+    render() {
+        return (
+            <div style={{color: this.props.theme.text}}>
+                <h1>Change Theme</h1>
+                <button type="button" className="btn btn-primary" onClick={() => this.handleLightTheme()}>Light theme</button>
+                <button type="button" className="btn btn-primary" onClick={() => this.handleDarkTheme()}>Dark theme</button>
+            </div>
+        )
+    }
+}
